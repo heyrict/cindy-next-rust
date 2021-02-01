14bdc21f31c6447a5d74d3428aea816c2d905475
heyrict <xiezh0831@yahoo.co.jp>
2021-02-01 18:56:37 +0800

    fix: Fix puzzle_footprint_count sql

--- src/sql/puzzle_footprint_count.sql
+++ src/sql/puzzle_footprint_count.sql
@@ -1,3 +1,3 @@
-SELECT COUNT(DISTINCT "dialogue"."id") AS count FROM "dialogue"
+SELECT COUNT(DISTINCT "puzzle"."id") AS count FROM "dialogue"
 INNER JOIN "puzzle" ON "dialogue"."puzzle_id" = "puzzle"."id"
 WHERE "dialogue"."user_id" = $1;
